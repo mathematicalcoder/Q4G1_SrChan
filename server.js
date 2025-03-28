22b1e1c28c2a2c4d2d7240c6a41057fd9854a655
mathemolympiad <kaiserchan1023@gmail.com>
2025-03-29 00:32:25 +0800

    song done

--- server.js
+++ server.js
@@ -30,12 +30,21 @@ app.get("/happy", (req, res) => {
 })
 
 app.post("/happy", (req, res) => {
+  // basic setup
   const data = req.body;
+  let output = "";
+  let song = "";
+  let counter = 0;
+  let coming = [];
+  // define songs
+  const song1 = ["Happy", "birthday", "to", "you", "Happy", "birthday", "to", "you", "Happy", "birthday", "dear", data.name, "Happy", "birthday", "to", "you!"]
+  let song2 = []
+  const song2Male = ["For", "he's", "a", "jolly", "good", "fellow", "For", "he's", "a", "jolly", "good", "fellow", "For", "he's", "a", "jolly", "good", "fellow", "which", "nobody", "can", "deny!"]
+  const song2Female = ["For", "she's", "a", "jolly", "good", "fellow", "For", "she's", "a", "jolly", "good", "fellow", "For", "she's", "a", "jolly", "good", "fellow", "which", "nobody", "can", "deny!"]
   if (!data.name || !data.gender) {
     // give error if name or gender is missing
     return res.status(400).send("At least one required field is missing!")
   } else {
-    let output = "";
     // add basic info
     output += `name: ${data.name}<br>
     gender: ${data.gender}<br>
@@ -45,10 +54,25 @@ app.post("/happy", (req, res) => {
       let nameId = `name${i+1}`
       let checkboxId = `checkbox${i+1}`
       output += `${nameId}: ${data[nameId]}<br>
-      ${checkboxId}: ${data[checkboxId]}`
+      ${checkboxId}: ${data[checkboxId]}<br>`
+      if (data[checkboxId] == "on") {
+        coming.push(data[nameId])
+      }
     }
+    // add song 1
+    for (word of song1) {
+      song += `${coming[counter]}: ${word}<br>`
+      counter += 1
+      if (counter == coming.length) {
+        counter = 0
+      }
+    }
+    if (data.gender == "male") {
+      song2 = song2Male
+    } else {song2 = song2Female}
+    song += `${coming[counter]}: ${song2}`
   }
-  res.render("happy.hbs", { output: output })
+  res.render("happy.hbs", { output: output, song })
 })
 
 //Makes the app listen to port 3000
